0d1067e47f3418781bdb3afb430bebc219c87c4d
Lakshay Kamat <93321969+lakshaykamat@users.noreply.github.com>
2023-06-23 00:29:34 +0530

    Navbar modified
    - icons size is increased to w-6 h-6
    - shadow added

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -5,22 +5,22 @@ type Props = {}
 
 const Navbar = (props: Props) => {
     return (
-        <nav className='bg-[#F0F0F0] flex justify-around py-3'>
+        <nav className='bg-[#F0F0F0] shadow-lg flex justify-around py-3'>
             <div>
                 <DTLOGO width='190' height='30' />
             </div>
             <div className='flex gap-3 first-letter:'>
                 <div className='bg-[#3683F0] p-2 rounded-[50%] '>
-                    <FaHome className='w-6 text-white h-6' />
+                    <FaHome className='w-5 text-white h-5' />
                 </div>
                 <div className='bg-[#3683F0] p-2 rounded-[50%] '>
-                    <FaGlasses className='w-6 text-white h-6' />
+                    <FaGlasses className='w-5 text-white h-5' />
                 </div>
                 <div className='bg-[#3683F0] p-2 rounded-[50%] '>
-                    <FaTools className='w-6 text-white h-6' />
+                    <FaTools className='w-5 text-white h-5' />
                 </div>
                 <div className='bg-[#3683F0] p-2 rounded-[50%] '>
-                    <FaBell className='w-6 text-white h-6' />
+                    <FaBell className='w-5 text-white h-5' />
                 </div>
             </div>
         </nav>
